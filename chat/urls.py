0b802a3f9b6073454182ada01e59575c46787c7f
Faisal Ahmed NSL <105410856+FaisalAhmed-NSL@users.noreply.github.com>
2024-07-04 16:56:22 +0600

    added static files

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("login/" , views.login_user, name="login"),
@@ -8,4 +9,4 @@ urlpatterns = [
     path('logout/', views.logout_user, name="logout")
     # path('peer1/', peer1, name='peer1'),
     # path('peer2/', peer2, name='peer2'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
