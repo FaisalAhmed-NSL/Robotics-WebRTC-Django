9e3195f2bca24c9a39fe5b15b2792cfae0a4fdef
Faisal Ahmed NSL <105410856+FaisalAhmed-NSL@users.noreply.github.com>
2024-07-04 17:45:38 +0600

    added static files

--- chat/routing.py
+++ chat/routing.py
@@ -5,5 +5,6 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'', consumers.ChatConsumer.as_asgi()),
     re_path(r'^peer[12]/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'chat/$', consumers.ChatConsumer.as_asgi()),
     
 ]
