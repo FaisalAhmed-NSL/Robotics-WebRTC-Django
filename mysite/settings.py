81535efac752fb71f97a13b9c9507a285a10e679
Faisal Ahmed NSL <105410856+FaisalAhmed-NSL@users.noreply.github.com>
2024-07-04 17:10:11 +0600

    added static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     "corsheaders",
 
     'chat.apps.ChatConfig',
@@ -68,7 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
     
 ]
 
