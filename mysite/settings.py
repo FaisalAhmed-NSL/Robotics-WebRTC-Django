2f3de41829929bd1a85fc47f9da0810c020c21da
Faisal Ahmed NSL <105410856+FaisalAhmed-NSL@users.noreply.github.com>
2024-07-09 14:44:38 +0600

    add redis local

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,9 +165,11 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            # "hosts": [('127.0.0.1', 6379)],
-            # "hosts": [os.environ.get('REDIS_URL','redis://localhost:6379')],
-            "hosts": [os.environ.get('REDIS_URL',default='redis://localhost:6379')],
+            "hosts": [('127.0.0.1', 6379)],
+            # "hosts": [('redis://localhost:6379')],
+            #  "hosts": [('redis://localhost:6379')],
+            #  "hosts": [(os.environ.get('REDIS_URL'))],
+            # "hosts": [os.environ.get('REDIS_URL',default='redis://localhost:6379')],
         },
     },
 }
